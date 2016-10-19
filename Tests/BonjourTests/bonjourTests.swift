c21551b0b79bda29a8a2be7998c27f8c4dc2aa58
Matthew Preucil <mattpsbs@gmail.com>
2016-10-18 17:06:25 -0700

    Don't dispatch to main from async context. Add tests.

--- Tests/BonjourTests/bonjourTests.swift
+++ Tests/BonjourTests/bonjourTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import Async
 @testable import Bonjour
 
 class BonjourTests: XCTestCase
@@ -16,6 +17,13 @@ class BonjourTests: XCTestCase
 		XCTAssertEqual(BuildServiceString(settings.serviceType, settings.serviceProtocol), "_testService._tcp")
     }
 
+	func testFind()
+	{
+		let broadcastScope = Bonjour.Broadcast(settings)
+		let services = await(Bonjour.FindAll(QuerySettings(serviceType: settings.serviceType, serviceProtocol: settings.serviceProtocol, domain: settings.domain)))
+		broadcastScope.dispose()
+		XCTAssertEqual(services.count, 1)
+	}
 
     static var allTests : [(String, (BonjourTests) -> () throws -> Void)]
 	{
