818f8b1c980a35fdc094cb2e940eb0ea65e793c4
Matthew Preucil <mattpsbs@gmail.com>
2016-11-21 19:27:51 -0800

    Remove www from Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Bonjour",
     dependencies: [
-		.Package(url: "https://www.github.com/randymarsh77/async", majorVersion: 1),
-		.Package(url: "https://www.github.com/randymarsh77/scope", majorVersion: 1),
+		.Package(url: "https://github.com/randymarsh77/async", majorVersion: 1),
+		.Package(url: "https://github.com/randymarsh77/scope", majorVersion: 1),
 	]
 )
